580ffc8d1943fc2b242a8ff4b7ebf3d3dfb083d2
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2021-07-17 00:37:49 -0600

    fix redirects

--- middleware/router-auth.js
+++ middleware/router-auth.js
@@ -5,10 +5,12 @@ export default function ({ window, app, store, redirect, route }) {
   app.i18n.setLocaleCookie(store.state.lang)
   app.i18n.setLocale(store.state.lang)
   if (route.path != '/' && store.state.modals.age && !isSigninRoute) {
-    return app.router.push('/')
+    return redirect('/')
+    // return app.router.push('/')
   }
 
   if (!isUserSignedIn && !isSigninRoute) {
-    return app.router.push('/signin')
+    return redirect('/signin')
+    // return app.router.push('/signin')
   }
 }
